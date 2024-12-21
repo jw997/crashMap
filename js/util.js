636906a4fd049b3cdbd2449d3fa1482d5e2fbaf8
jonathan <jonathan_walden@hotmail.com>
2024-12-21 12:34:56 -0800

    Initial commit

---
+++ js/util.js
@@ -0,0 +1,28 @@
+
+import {getJson} from "./utils_helper.js";
+
+
+
+const collisionsJsonFile = './db/exports/BPCOLL.json';
+const collisionsJson = await getJson(collisionsJsonFile);
+
+const transparencyJsonFile = './db/exports/transparency.portal.2024.json';
+const transparencyJson = await getJson(transparencyJsonFile);
+
+async function getTransparencyData() {
+	var arrays = [];
+	for (var y = 2010; y<=2024; y++ ) {
+		const file = './data/' + y + '.json';
+		const transparencyJson = await getJson(file);
+		arrays.push(transparencyJson.features);
+	}
+	const retval = [].concat(...arrays)
+	return retval;
+
+}
+
+const mergedTransparencyJson = await(getTransparencyData());
+
+
+
+export {collisionsJson,transparencyJson, mergedTransparencyJson};
