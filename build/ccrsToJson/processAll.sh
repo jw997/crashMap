153d601eeda987d5457de5535c1cdfe81143b34c
jonathan <jonathan_walden@hotmail.com>
2025-11-17 18:02:23 -0800

    automate ccrs download steps

--- build/ccrsToJson/processAll.sh
+++ build/ccrsToJson/processAll.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
+set -e
+echo "Removing downloaded and generated files"
+mkdir -p temp/output
+mkdir output
+rm temp/urls temp/toc.html || true
+rm temp/*.csv || true;
+rm temp/output/* || true;
 
-sed -i 's/\t//g' 20*crashes.csv 
+wget -O temp/toc.html https://data.ca.gov/dataset/ccrs
+
+grep schema:url temp/toc.html  | grep -o 'http.*csv' | sort  -t '/' -k 8  > temp/urls
+
+#download all urls to temp
+wget -P temp -i temp/urls
+
+rename 's/hq1d-p-app52dopendataexport//' temp/hq1d-p-app52dopendataexport20*
+
+sed -i 's/\t//g' temp/20*crashes.csv 
+
+for year in {2016..2025}; do
+  echo "check files for year ${year}"
+  if [ ! -f temp/${year}crashes.csv ]; then
+    echo "Expected file temp/${year}crashes.csv not found" 
+  fi
+  if [ ! -f temp/${year}parties.csv ]; then
+    echo "Expected file temp/${year}parties.csv not found" 
+  fi
+  if [ ! -f temp/${year}injuredwitnesspassengers.csv ]; then
+    echo "Expected file temp/${year}injuredwitnesspassengers.csv not found" 
+  fi    
+done
 
 for year in {2016..2025}; do
   echo "Processing year ${year}"
-  node ccrsToJson.js  ${year}crashes.csv ${year}parties.csv ${year}iwp.csv  ccrs${year}.json
+  node  ccrsToJson.js  temp/${year}crashes.csv temp/${year}parties.csv temp/${year}injuredwitnesspassengers.csv  temp/output/ccrs${year}.json | tee temp/log${year}
 done
-#exit 
-#node ccrsToJson.js  2025crashes.csv 2025parties.csv 2025iwp.csv  ccrs2025.json
-#node ccrsToJson.js  2024crashes.csv 2024parties.csv 2024iwp.csv  ccrs2024.json
-#node ccrsToJson.js  2023crashes.csv 2023parties.csv 2023iwp.csv  ccrs2023.json
-#node ccrsToJson.js  2022crashes.csv 2022parties.csv 2022iwp.csv  ccrs2022.json
-#node ccrsToJson.js  2021crashes.csv 2021parties.csv 2021iwp.csv  ccrs2021.json
-#node ccrsToJson.js  2020crashes.csv 2020parties.csv 2020iwp.csv  ccrs2020.json
-#node ccrsToJson.js  2019crashes.csv 2019parties.csv 2019iwp.csv  ccrs2019.json
-#node ccrsToJson.js  2018crashes.csv 2018parties.csv 2018iwp.csv  ccrs2018.json
-#node ccrsToJson.js  2017crashes.csv 2017parties.csv 2017iwp.csv  ccrs2017.json
-#node ccrsToJson.js  2016crashes.csv 2016parties.csv 2016iwp.csv  ccrs2016.json
 
-
-
+echo "look for output in temp/ouput/"
+echo "bye"
 
